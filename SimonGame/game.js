297d54966e86c0f9b00c7385a0844dd14cbad1c2
Himansh Singh <himansh.singh3@gmail.com>
2021-05-13 09:43:17 +0530

    update: simon game

--- SimonGame/game.js
+++ SimonGame/game.js
@@ -4,47 +4,81 @@ let sequence = []
 
 let roundNum = 0;
 let lost = false
+let startGame = true
 let seqNum = 0;
+
+// To start or restart the game 
+$(document).on("keydown", function () {
+    if (startGame || lost) {
+        startGame = false;
+        roundNum = 0;
+        lost = false;
+        sequence = []
+        nextRound();
+    }
+})
+
+$(".btn").on("click", function () {
+
+    let userChoseColor = this.id
+    animatePressed(userChoseColor)
+    playSound(userChoseColor)
+
+    checkAnswer(userChoseColor)
+})
+
+function checkAnswer(userChoseColor) {
+    if (lost || userChoseColor != sequence[seqNum]) {
+        gameOver()
+        playSound("wrong")
+        return
+    }
+
+    seqNum++;
+    if (seqNum === sequence.length) {
+        setTimeout(function() {
+            nextRound()
+        },500)
+    }
+}
 
 function nextSequence() {
     let randomNum = Math.floor((Math.random() * 4))
     sequence.push(colors[randomNum])
 
     $("#" + colors[randomNum]).fadeOut(200).fadeIn(200).fadeOut(100).fadeIn(100);
-    new Audio("./sounds/" + colors[randomNum] + ".mp3").play();
+    playSound(colors[randomNum])
 
     console.log(sequence)
 }
 
-$(document).on("keydown", function () {
-    nextRound()
-})
-
-$(".btn").on("click", function () {
-
-    $("#" + this.id).fadeOut(100).fadeIn(100);
-
-    let userChoseColor = this.id
-    if (lost || userChoseColor != sequence[seqNum]) {
-        $("#level-title").text("Game Over, Press Any Key to Restart")
-        new Audio("./sounds/wrong.mp3").play()
-        
-        // resetting the game
-        lost = true
-        sequence = []
-        return
-    }
-
-    seqNum++;
-    if (seqNum === sequence.length) {
-        nextRound()
-    }
-})
-
 function nextRound() {
     roundNum++;
-    $("#level-title").text("Round : " + roundNum)
+    $("#level-title").text("Round : " + roundNum);
     nextSequence();
     seqNum = 0;
     lost = false;
+}
+
+function gameOver() {
+    $("#level-title").text("Game Over, Press Any Key to Restart")
+    lost = true
+
+    $("body").addClass("game-over")
+    setTimeout(function() {
+        $("body").removeClass("game-over");
+    },200)
+}
+
+function playSound(name) {
+    new Audio("./sounds/" + name + ".mp3").play();
+}
+
+function animatePressed(name) {
+
+    $("#" + name).addClass("pressed");
+    setTimeout(function() {
+        $("#" + name).removeClass("pressed");
+    },100)
+
 }
