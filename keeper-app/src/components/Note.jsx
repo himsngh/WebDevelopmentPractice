93a142b3c4f44d1a670754f06ccb1ff39a4d2a31
Himansh Singh <himansh.singh3@gmail.com>
2021-06-21 10:39:01 +0530

    inital commit keeper-app react

---
+++ keeper-app/src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Note() {
+    return (
+        <div className="note">
+            <h1> Note Title </h1>
+            <p1> Note Content </p1>
+        </div>
+    )
+}
+
+export default Note;
