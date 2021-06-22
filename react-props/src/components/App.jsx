3ffd02c71c54b3ef292bd8b7bb5d69200acb0401
Himansh Singh <himansh.singh3@gmail.com>
2021-06-22 10:44:42 +0530

    add: map arrays of objects to create React components

--- react-props/src/components/App.jsx
+++ react-props/src/components/App.jsx
@@ -7,9 +7,26 @@ function App() {
     return (
         <div>
             <h1 className="heading">My Contacts</h1>
-            <Card name={contacts[0].name} src={contacts[0].imgURL} phone={contacts[0].phone} email={contacts[0].email} />
+            {
+                contacts.map((value) => {
+                    return <Card
+                        key={value.id}
+                        name={value.name}
+                        src={value.imgURL}
+                        phone={value.phone}
+                        email={value.email}
+                    />
+                })
+                //  contacts.map((value) => (
+                //     <Card name={value.name} src={value.imgURL} phone={value.phone} email={value.email} />
+                // ))
+                // instead of curly braces and returning the card explicitely we can direclty write normal brackets and directly the return
+                // part and no need to explicitely return it 
+
+            }
+            {/* <Card name={contacts[0].name} src={contacts[0].imgURL} phone={contacts[0].phone} email={contacts[0].email} />
             <Card name={contacts[1].name} src={contacts[1].imgURL} phone={contacts[1].phone} email={contacts[1].email} />
-            <Card name={contacts[2].name} src={contacts[2].imgURL} phone={contacts[2].phone} email={contacts[2].email} />
+            <Card name={contacts[2].name} src={contacts[2].imgURL} phone={contacts[2].phone} email={contacts[2].email} /> */}
         </div>
     )
 }
